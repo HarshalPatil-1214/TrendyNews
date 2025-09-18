87d2b3897905d0faa4f53d5364d6e8c4c770b9a7
HarshalPatil-1214 <patilharshal1214@gmail.com>
2025-09-18 10:08:32 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TrendyNews/',   // 👈 repo name here
+  base: '/TrendyNews/',   // 👈 use your repo name here
   plugins: [react()],
 })
 
